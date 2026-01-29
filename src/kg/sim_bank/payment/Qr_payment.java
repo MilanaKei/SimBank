43444f61d0e596486dae6dfbc2b2523218662f39
Milana Kubatbek kyzy <milanakubatbekk@192.168.0.100>
2026-01-29 21:24:25 +0600

    added sout and VisaPayment class

--- src/kg/sim_bank/payment/Qr_payment.java
+++ src/kg/sim_bank/payment/Qr_payment.java
@@ -3,5 +3,9 @@ package kg.sim_bank.payment;
 public class Qr_payment {
     public static void main(String[] args) {
         System.out.println("QR Payment");
+        System.out.println("Hello world");
     }
 }
+
+// git init
+//
