63b94e3db2f6acb7cec1bea38f389a63b00f995e
Milana Kubatbek kyzy <milanakubatbekk@192.168.0.104>
2026-02-04 23:29:45 +0600

    Resolved conflict

---
+++ src/kg/sim_bank/delivery/Delivery.java
@@ -0,0 +1,11 @@
+package kg.sim_bank.delivery;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Delivery {
+    public static void main(String[] args) {
+
+        System.out.println("Delivery Yandex");
+        System.out.println("Delivery Glovo");
+    }
+}
